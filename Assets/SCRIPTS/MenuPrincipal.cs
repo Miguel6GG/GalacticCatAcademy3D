1f76864c0809da52648db79c52ef2282c7ef5785
Miguel6GG <miggargar6@alu.edu.gva.es>
2024-02-15 08:57:03 +0100

    Codigo MenuPrincipal y función Exit
    Añadido el código del menu principal y la funcion para salir del juego al darle al botón

---
+++ Assets/SCRIPTS/MenuPrincipal.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuPrincipal : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
