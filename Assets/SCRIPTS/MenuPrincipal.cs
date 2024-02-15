962d29ebf3d7fc447e7a3f95ff34825b4e428cb4
Miguel6GG <miggargar6@alu.edu.gva.es>
2024-02-15 10:02:34 +0100

    MenuNiveles y transiciones
    Cambios:
    -Se ha añadido un menu de niveles
    -Al darle al boton de "Niveles" se hará una animación slide al menu Niveles y viceversa con el botón de "Atrás"
    -Añadido LeenTween para animaciones

--- Assets/SCRIPTS/MenuPrincipal.cs
+++ Assets/SCRIPTS/MenuPrincipal.cs
@@ -4,12 +4,27 @@ using UnityEngine;
 
 public class MenuPrincipal : MonoBehaviour
 {
+    public GameObject Menu;
+    public GameObject MenuNiveles;
+
     void Start()
     {
         
     }
 
-    public void Exit()
+    public void IrMenuPrincipal()
+    {
+        LeanTween.moveX(Menu.GetComponent<RectTransform>(), 0, 1.5f);
+        LeanTween.moveX(MenuNiveles.GetComponent<RectTransform>(), 1000, 1.5f);
+    }
+
+    public void IrNiveles()
+    {
+        LeanTween.moveX(Menu.GetComponent<RectTransform>(), -1000, 1.5f);
+        LeanTween.moveX(MenuNiveles.GetComponent<RectTransform>(), 0, 1.5f);
+    }
+
+    public void Salir()
     {
         Application.Quit();
     }
