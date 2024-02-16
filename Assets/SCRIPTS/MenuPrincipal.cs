934267e280d44dc2f56b91c3ccd85f1ba4398cbe
Miguel6GG <miggargar6@alu.edu.gva.es>
2024-02-16 13:36:12 +0100

    Movimiento Menus
    Los menus ahora se cambian sin problema

--- Assets/SCRIPTS/MenuPrincipal.cs
+++ Assets/SCRIPTS/MenuPrincipal.cs
@@ -17,12 +17,27 @@ public class MenuPrincipal : MonoBehaviour
     {
         LeanTween.moveX(Menu.GetComponent<RectTransform>(), 0, 1f).setEase(LeanTweenType.easeInOutBack);
         LeanTween.moveX(MenuNiveles.GetComponent<RectTransform>(), 1000, 1f).setEase(LeanTweenType.easeInOutBack);
+        LeanTween.moveX(MenuOpciones.GetComponent<RectTransform>(), 1000, 1f).setEase(LeanTweenType.easeInOutBack);
     }
 
     public void IrNiveles()
     {
         LeanTween.moveX(Menu.GetComponent<RectTransform>(), -1000, 1f).setEase(LeanTweenType.easeInOutBack);
         LeanTween.moveX(MenuNiveles.GetComponent<RectTransform>(), 0, 1f).setEase(LeanTweenType.easeInOutBack);
+        LeanTween.moveX(MenuOpciones.GetComponent<RectTransform>(), 0, 1f).setEase(LeanTweenType.easeInOutBack);
+
+        LeanTween.moveY(MenuNiveles.GetComponent<RectTransform>(), 0, 0f).setEase(LeanTweenType.easeInOutBack);
+        LeanTween.moveY(MenuOpciones.GetComponent<RectTransform>(), -600, 0f).setEase(LeanTweenType.easeInOutBack);
+    }
+
+    public void IrOpciones()
+    {
+        LeanTween.moveX(Menu.GetComponent<RectTransform>(), -1000, 1f).setEase(LeanTweenType.easeInOutBack);
+        LeanTween.moveX(MenuNiveles.GetComponent<RectTransform>(), 0, 1f).setEase(LeanTweenType.easeInOutBack);
+        LeanTween.moveX(MenuOpciones.GetComponent<RectTransform>(), 0, 1f).setEase(LeanTweenType.easeInOutBack);
+
+        LeanTween.moveY(MenuNiveles.GetComponent<RectTransform>(), -600, 0f).setEase(LeanTweenType.easeInOutBack);
+        LeanTween.moveY(MenuOpciones.GetComponent<RectTransform>(), 0, 0f).setEase(LeanTweenType.easeInOutBack);
     }
 
     public void Salir()
