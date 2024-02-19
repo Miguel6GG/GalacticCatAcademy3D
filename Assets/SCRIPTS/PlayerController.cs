26dfcf3083e57b5ca4b98a45aa41ca225a06378d
Manuel <manueldelarosa1340@gmail.com>
2024-02-19 13:57:06 +0100

    mas cosas para bartolomeow añadidas y pez

--- Assets/SCRIPTS/PlayerController.cs
+++ Assets/SCRIPTS/PlayerController.cs
@@ -10,6 +10,7 @@ public class CharacterMovement : MonoBehaviour
     Animator animator;
 
     float verticalSpeed;
+    Vector3 lastMovementDirection;
 
     private void Awake()
     {
@@ -22,13 +23,23 @@ public class CharacterMovement : MonoBehaviour
         if (controller.isGrounded)
         {
             if (Input.GetButtonDown("Jump"))
+            {
                 verticalSpeed = jumpForce;
+                animator.SetTrigger("Jump");
+            }
             else
+            {
                 verticalSpeed = -1;
+                if (lastMovementDirection.magnitude > 0)
+                    animator.SetBool("IsWalking", true);
+                else
+                    animator.SetBool("IsWalking", false);
+            }
         }
         else
         {
             verticalSpeed -= gravity * Time.deltaTime;
+            animator.SetBool("IsWalking", false);
         }
 
         float horizontalInput = Input.GetAxisRaw("Horizontal");
@@ -36,5 +47,14 @@ public class CharacterMovement : MonoBehaviour
 
         Vector3 movement = new Vector3(horizontalInput * movementSpeed, verticalSpeed, verticalInput * movementSpeed) * Time.deltaTime;
         controller.Move(movement);
+
+        // Rotate the character
+        if (horizontalInput != 0 || verticalInput != 0)
+        {
+            Vector3 moveDirection = new Vector3(horizontalInput, 0, verticalInput);
+            Quaternion newRotation = Quaternion.LookRotation(moveDirection);
+            transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, Time.deltaTime * 10);
+            lastMovementDirection = moveDirection;
+        }
     }
 }
